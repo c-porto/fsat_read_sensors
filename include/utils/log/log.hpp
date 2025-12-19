fc6fa53890480b386299255b2cdf0a4b6c601ac4
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2025-12-19 00:33:36 -0300

    Remove `log` directory from gitignore

---
+++ include/utils/log/log.hpp
@@ -0,0 +1,58 @@
+#ifndef LOG_HPP_
+#define LOG_HPP_
+
+#include <cstdarg>
+#include <cstdlib>
+#include <mutex>
+#include <string>
+
+enum LogLevel {
+	NONE = -1,
+	LOG = 0,
+	INFO,
+	WARN,
+	ERR,
+	DEBUG,
+};
+
+namespace logs
+{
+inline std::string logFile;
+inline std::string dataFile;
+inline bool disableJournal = false;
+inline bool disableFileLogs = false;
+inline bool coloredLogs = true;
+inline std::recursive_mutex logMutex;
+
+void init(std::string logDir);
+void log(const LogLevel level, std::string str);
+
+void logSensorData(std::string &sensorName, std::string &type, double data);
+
+inline void log(const LogLevel level, const char *fmt, ...)
+{
+	std::lock_guard<std::recursive_mutex> guard{ logMutex };
+	std::string logMsg = "";
+	char *allocFmt;
+	va_list args;
+
+	va_start(args, fmt);
+
+	if (vasprintf(&allocFmt, fmt, args) < 0) {
+		log(ERR, "Failed to allocated log string!!");
+		va_end(args);
+		return;
+	}
+
+	va_end(args);
+
+	logMsg += allocFmt;
+
+	free(allocFmt);
+
+	log(level, logMsg);
+}
+
+}
+
+#endif
---
+++ utils/log/log.cpp
@@ -0,0 +1,60 @@
+#include <sys/syslog.h>
+#include <systemd/sd-journal.h>
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <utils/log/log.hpp>
+
+void logs::init(std::string log_dir) {
+  logFile = log_dir + "/fsat-sens.log";
+
+  if (!std::filesystem::exists(log_dir))
+    std::filesystem::create_directories(log_dir);
+}
+
+void logs::log(const LogLevel level, std::string str) {
+  std::lock_guard<std::recursive_mutex> guard{logMutex};
+  std::string coloredStr = str;
+  std::string out = str;
+  int sysLog;
+
+  switch (level) {
+    case LOG:
+      out = "[LOG] " + str;
+      coloredStr = str;
+      sysLog = LOG_NOTICE;
+      break;
+    case WARN:
+      out = "[WARN] " + str;
+      coloredStr = "\033[1;33m" + str + "\033[0m";  // yellow
+      sysLog = LOG_WARNING;
+      break;
+    case ERR:
+      out = "[ERR] " + str;
+      coloredStr = "\033[1;31m" + str + "\033[0m";  // red
+      sysLog = LOG_ERR;
+      break;
+    case INFO:
+      out = "[INFO] " + str;
+      coloredStr = "\033[1;32m" + str + "\033[0m";  // green
+      sysLog = LOG_INFO;
+      break;
+    case DEBUG:
+      out = "[DEBUG] " + str;
+      coloredStr = "\033[1;34m" + str + "\033[0m";  // blue
+      sysLog = LOG_DEBUG;
+      break;
+    default:
+      break;
+  }
+
+  if (!disableFileLogs) {
+    std::ofstream ofs;
+    ofs.open(logFile, std::ios::out | std::ios::app);
+    ofs << out << "\n";
+    ofs.close();
+  }
+
+  if (!disableJournal) std::cout << ((coloredLogs) ? coloredStr : str) << '\n';
+}
